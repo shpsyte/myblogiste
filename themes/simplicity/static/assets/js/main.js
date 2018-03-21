6b17cc757b7a287c470ca1b6467fad8f039e7e36
José Luiz <jose.iscosistemas@outlook.com>
2018-03-21 14:12:39 -0300

    redirecionametn bot

--- themes/simplicity/static/assets/js/main.js
+++ themes/simplicity/static/assets/js/main.js
@@ -6,6 +6,7 @@ function Opneva() {
      t=window.innerHeight-height;
      l=window.innerWidth-width-10;
      
+      window.open('https://bots4app.azurewebsites.net/chat.html?appid=0012ss22sfs23333&avatar=avatarJose.png&botname=José Luiz - Virtual&placeholder=Ex. Pesquise sobre C#&userid=Visitante&s=ShkEQ9oIn4w.cwA.6mA.m1LIQ4xMWORfAl6r3MJNqoO0uwnzz5qKxmH09PMC61U&css=botconfig ', '', 'height=' + height + ', width=' + width + ', left=' + l + ', top=' + t);
 
       window.open('/chat', '', 'height='+height+', width='+width+', left='+l+', top='+t );
 
